87b76569286c9a0e42ea0e4327400e1c29d7548c
fathsbs <55565303+fathsbs@users.noreply.github.com>
2019-09-23 18:58:54 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan1-Biodata/src/IF0110118039Latihan1Biodata.java
@@ -0,0 +1,25 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM : Program yang berisi program yang dapat menampilkan biodata pribadi saya  
+ */
+
+public class IF0110118039Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------");
+        System.out.println("NAMA\t\t: Fatahillah Satria Bima Seno");
+        System.out.println("ALAMAT\t\t: Jl. Tubagus Ismail Dalam No.48");
+        System.out.println("TEMPAT LAHIR\t: Jakarta");
+        System.out.println("TANGGAL LAHIR\t: 02 November 1998");
+        System.out.println("----------------");
+        System.out.println("HOBI\t\t: Membaca Novel dan Komik serta Berlari");
+        System.out.println("MAKES\t\t: Singkong");
+        System.out.println("MIKES\t\t: Teh Pahit");
+    }
+
+}
